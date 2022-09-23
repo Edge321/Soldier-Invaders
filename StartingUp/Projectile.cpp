21599042e9fa46475f796ac89ca52d7fbdcce6a9
Edge321 <pallares00@outlook.com>
2022-09-22 23:10:18 -0600

    Small update to main for shooting

--- StartingUp/Projectile.cpp
+++ StartingUp/Projectile.cpp
@@ -1,1 +1,12 @@
 #include "Projectile.h"
+
+Projectile::Projectile() {}
+Projectile::~Projectile() {}
+
+void Projectile::update(float dt) {
+
+}
+
+void Projectile::rotate(float dt, float angleChange) {
+	m_sprite.setRotation(m_sprite.getRotation() + (dt * angleChange));
+}
